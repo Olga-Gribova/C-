069d187023307c2e257dcfdeb8fdab09e48e4471
Olga-Gribova <43218492+Olga-Gribova@users.noreply.github.com>
2019-06-09 15:02:08 +0300

    T+-TI, TI+-TI, T-T

---
+++ time.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+//#include <conio.h>
+
+const int SIZE = 5;
+int LIMIT[SIZE] = { 60, 24, 30, 11, 10000};
+
+using namespace std;
+
+class Time {
+public:
+	string h, m, dd, mm, yy;
+	Time() {
+		h = "00"; m = "00"; dd = "00"; mm = "00"; yy = "0000";
+	}
+	Time(const string & h_0, const string & m_0, const string & dd_0, const string & mm_0, const string & yy_0) {
+		h = h_0; m = m_0; dd = dd_0; mm = mm_0; yy = yy_0;
+	}
+	void show() {
+		cout << h << ":" << m << " " << dd << "/" << mm << "/" << yy << endl;;
+	}
+};
+
+class TimeInterval {
+public:
+	string h, m, dd, mm, yy;
+	TimeInterval() {
+		h = "00"; m = "00"; dd = "00"; mm = "00"; yy = "0000";
+	}
+	TimeInterval(const string & h_0, const string & m_0, const string & dd_0, const string & mm_0, const string & yy_0) {
+		h = h_0; m = m_0; dd = dd_0; mm = mm_0; yy = yy_0;
+	}
+	void show() {
+		cout << h << " hours, " << m << " minutes, " << dd << " days, " << mm << " months, " << yy << " years" << endl;;
+	}
+};
+
+Time operator+(Time & t, TimeInterval & ti) {
+	int m = 0;
+	string result[SIZE];
+	//int  M = stoi(t.m) + stoi(ti.m), H = stoi(t.h) + stoi(ti.h), MM = stoi(t.mm) + stoi(ti.mm), DD = stoi(t.dd) + stoi(ti.dd), YY = stoi(t.yy) + stoi(ti.yy);
+	int time[SIZE]= {stoi(t.m) + stoi(ti.m), stoi(t.h) + stoi(ti.h), stoi(t.dd) + stoi(ti.dd), stoi(t.mm) + stoi(ti.mm), stoi(t.yy) + stoi(ti.yy)};
+	for (int i = 0; i < SIZE - 1; i++) {
+		if (time[i] >= LIMIT[i]) {
+			m = time[i] / LIMIT[i];
+			time[i+1] = time[i+1] + m;
+			time[i] -= m * time[i];
+			if (i == 2 || i == 3)
+				time[i]++;
+			if (time[i] < 10)
+				result[i] = "0" + to_string(time[i]);
+			else
+				result[i] = to_string(time[i]);
+		}
+		if (time[i] < 10)
+			if (time[i] == 0)
+				result[i] = "00";
+			else
+				result[i] = "0" + to_string(time[i]);
+		else
+			result[i] = to_string(time[i]);
+	}
+	if (time[4] >= LIMIT[4])
+	{
+		m = time[4] / LIMIT[4];
+		time[4] -= m * time[4];
+	}
+	if (time[4] == 0)
+		result[4] = "0000";
+	else if (time[4] < 10)
+		result[4] = "0" + to_string(time[4]);
+	else if (time[4] < 100)
+		result[4] = "00" + to_string(time[4]);
+	else if (time[4] < 1000)
+		result[4] = "000" + to_string(time[4]);
+	else
+		result[4] = to_string(time[4]);
+	Time t1(result[1], result[0], result[2], result[3], result[4]);
+	return t1;
+}
+
+TimeInterval operator+(TimeInterval & t, TimeInterval & ti) {
+	int m = 0;
+	string result[SIZE];
+	//int  M = stoi(t.m) + stoi(ti.m), H = stoi(t.h) + stoi(ti.h), MM = stoi(t.mm) + stoi(ti.mm), DD = stoi(t.dd) + stoi(ti.dd), YY = stoi(t.yy) + stoi(ti.yy);
+	int time[SIZE]= {stoi(t.m) + stoi(ti.m), stoi(t.h) + stoi(ti.h), stoi(t.dd) + stoi(ti.dd), stoi(t.mm) + stoi(ti.mm), stoi(t.yy) + stoi(ti.yy)};
+	for (int i = 0; i < SIZE - 1; i++) {
+		if (time[i] >= LIMIT[i]) {
+			m = time[i] / LIMIT[i];
+			time[i+1] = time[i+1] + m;
+			time[i] -= m * time[i];
+			if (i == 2 || i == 3)
+				time[i]++;
+			if (time[i] < 10)
+				result[i] = "0" + to_string(time[i]);
+			else
+				result[i] = to_string(time[i]);
+		}
+		if (time[i] < 10)
+			if (time[i] == 0)
+				result[i] = "00";
+			else
+				result[i] = "0" + to_string(time[i]);
+		else
+			result[i] = to_string(time[i]);
+	}
+	if (time[4] >= LIMIT[4])
+	{
+		m = time[4] / LIMIT[4];
+		time[4] -= m * time[4];
+	}
+	if (time[4] == 0)
+		result[4] = "0000";
+	else if (time[4] < 10)
+		result[4] = "0" + to_string(time[4]);
+	else if (time[4] < 100)
+		result[4] = "00" + to_string(time[4]);
+	else if (time[4] < 1000)
+		result[4] = "000" + to_string(time[4]);
+	else
+		result[4] = to_string(time[4]);
+	TimeInterval t1(result[1], result[0], result[2], result[3], result[4]);
+	return t1;
+}
+
+Time operator-(Time & t, TimeInterval & ti) {
+	string result[SIZE];
+	int time[SIZE]= {stoi(t.m) - stoi(ti.m), stoi(t.h) - stoi(ti.h), stoi(t.dd) - stoi(ti.dd), stoi(t.mm) - stoi(ti.mm), stoi(t.yy) - stoi(ti.yy)};
+	for (int i = 0; i < SIZE - 1; i++) {
+		if (time[i] < 0) {
+			time[i] += LIMIT[i];
+			time[i+1]--;
+			if (i == 2 || i == 3)
+				time[i]++;
+			if (time[i] < 10)
+				result[i] = "0" + to_string(time[i]);
+			else
+				result[i] = to_string(time[i]);
+		}
+		if (time[i] < 10)
+			if (time[i] == 0)
+				result[i] = "00";
+			else
+				result[i] = "0" + to_string(time[i]);
+		else
+			result[i] = to_string(time[i]);
+	}
+	if (time[4] <= 0)
+		cout << "years BC" << endl;
+	if (time[4] == 0)
+		result[4] = "0000";
+	else if (time[4] < 10)
+		result[4] = "0" + to_string(time[4]);
+	else if (time[4] < 100)
+		result[4] = "00" + to_string(time[4]);
+	else if (time[4] < 1000)
+		result[4] = "000" + to_string(time[4]);
+	else
+		result[4] = to_string(time[4]);
+	Time t1(result[1], result[0], result[2], result[3], result[4]);
+	return t1;
+}
+TimeInterval operator-(TimeInterval & t, TimeInterval & ti) {
+	string result[SIZE];
+	int time[SIZE]= {stoi(t.m) - stoi(ti.m), stoi(t.h) - stoi(ti.h), stoi(t.dd) - stoi(ti.dd), stoi(t.mm) - stoi(ti.mm), stoi(t.yy) - stoi(ti.yy)};
+	for (int i = 0; i < SIZE - 1; i++) {
+		if (time[i] < 0) {
+			time[i] += LIMIT[i];
+			time[i+1]--;
+			if (i == 2 || i == 3)
+				time[i]++;
+			if (time[i] < 10)
+				result[i] = "0" + to_string(time[i]);
+			else
+				result[i] = to_string(time[i]);
+		}
+		if (time[i] < 10)
+			if (time[i] == 0)
+				result[i] = "00";
+			else
+				result[i] = "0" + to_string(time[i]);
+		else
+			result[i] = to_string(time[i]);
+	}
+	if (time[4] <= 0)
+		cout << "years BC" << endl;
+	if (time[4] == 0)
+		result[4] = "0000";
+	else if (time[4] < 10)
+		result[4] = "0" + to_string(time[4]);
+	else if (time[4] < 100)
+		result[4] = "00" + to_string(time[4]);
+	else if (time[4] < 1000)
+		result[4] = "000" + to_string(time[4]);
+	else
+		result[4] = to_string(time[4]);
+	TimeInterval t1(result[1], result[0], result[2], result[3], result[4]);
+	return t1;
+}
+TimeInterval operator-(Time & t, Time & ti) {
+	string result[SIZE];
+	int time[SIZE]= {stoi(t.m) - stoi(ti.m), stoi(t.h) - stoi(ti.h), stoi(t.dd) - stoi(ti.dd), stoi(t.mm) - stoi(ti.mm), stoi(t.yy) - stoi(ti.yy)};
+	for (int i = 0; i < SIZE - 1; i++) {
+		if (time[i] < 0) {
+			time[i] += LIMIT[i];
+			time[i+1]--;
+			if (i == 2 || i == 3)
+				time[i]++;
+			if (time[i] < 10)
+				result[i] = "0" + to_string(time[i]);
+			else
+				result[i] = to_string(time[i]);
+		}
+		if (time[i] < 10)
+			if (time[i] == 0)
+				result[i] = "00";
+			else
+				result[i] = "0" + to_string(time[i]);
+		else
+			result[i] = to_string(time[i]);
+	}
+	if (time[4] <= 0)
+		cout << "years BC" << endl;
+	if (time[4] == 0)
+		result[4] = "0000";
+	else if (time[4] < 10)
+		result[4] = "0" + to_string(time[4]);
+	else if (time[4] < 100)
+		result[4] = "00" + to_string(time[4]);
+	else if (time[4] < 1000)
+		result[4] = "000" + to_string(time[4]);
+	else
+		result[4] = to_string(time[4]);
+	TimeInterval t1(result[1], result[0], result[2], result[3], result[4]);
+	return t1;
+}
+
+int main() {
+	Time t1("13", "45", "01", "06", "2019"), t2;
+	TimeInterval ti1("01", "07", "21", "01", "0001"), ti2("13", "45", "01", "06", "2019"), ti3;
+	cout << "time1: ";
+	//t1.show();
+	ti2.show();
+	cout << "time2: ";
+	ti1.show();
+	cout << "sum: ";
+	//t2 = t1 + ti1;
+	//t2.show();
+	ti3 = ti2 + ti1;
+	ti3.show();
+	cout << "dif: ";
+	//t2 = ti2 - ti1;
+	//t2.show();
+	ti3 = ti2 - ti1;
+	ti3.show();
+//	_getch();
+	return 0;
+}
